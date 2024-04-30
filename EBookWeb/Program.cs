b801bf61f21c03d853eef05c04607e032027c223
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-04-30 20:42:40 +0100

    installer package d'entity  framwork tools, et pucher la table category to databaseSetup Program.cs to use DbContext

--- EBookWeb/Program.cs
+++ EBookWeb/Program.cs
@@ -1,8 +1,16 @@
+using EBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
