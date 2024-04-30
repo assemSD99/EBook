39f6a7dad143d6a81b7ddffed41970f08d472dab
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-04-30 20:32:38 +0100

    Add ApplicationDbContext avec son package entity framwork core

---
+++ EBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
