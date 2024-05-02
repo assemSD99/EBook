7be7de55eb7f9164494b629572896f1b0d97a9c5
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-02 14:52:43 +0100

    Lister tous les categories mathode Index

--- EBookWeb/Controllers/CategoryController.cs
+++ EBookWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var category = _context.Categories.ToList();
             return View();
         }
     }
