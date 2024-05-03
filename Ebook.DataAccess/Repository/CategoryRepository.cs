faf0bf9f8408e4e0aa74c901c7ae46dfb9e7dfb4
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-03 17:50:41 +0100

    Implement Category Repository

---
+++ Ebook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Ebook.DataAccess.Repository.IRepository;
+using Ebook.Models;
+using EBook.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebook.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
