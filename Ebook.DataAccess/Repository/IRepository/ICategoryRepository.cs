d2a9df0528ae15dd0a0c78ebeecc48b7c72c9778
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-03 17:42:39 +0100

    Category Repository Interface

---
+++ Ebook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void save();
+    }
+}
