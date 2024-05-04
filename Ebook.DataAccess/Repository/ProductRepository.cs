d3a2d482b7ce9672687eab73926bb905bb1191ee
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-04 13:26:50 +0100

    Product Repository

---
+++ Ebook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Ebook.DataAccess.Repository.IRepository;
+using Ebook.Models;
+using EBook.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private  ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(i  => i.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.Author = obj.Author;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.ISBN= obj.ISBN;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+            
+            
+        }
+    }
+}
