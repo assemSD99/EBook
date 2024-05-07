6f2fb1607a8ac8be14ffe0c68e820b7f32ac39f1
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-07 19:05:05 +0100

    Details Action method

--- EBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ EBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Ebook.DataAccess.Repository;
 using Ebook.DataAccess.Repository.IRepository;
 using Ebook.Models;
+using Ebook.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -23,6 +24,17 @@ public class HomeController : Controller
         IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
         return View(productList);
     }
+
+    public IActionResult Details(int id)
+    {
+        ShoppingCart objCart = new()
+        {
+            Count = 1,
+            Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+        };
+		
+		return View(objCart);
+	}
 
     public IActionResult Privacy()
     {
