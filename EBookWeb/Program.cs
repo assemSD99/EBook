63af4a99844ab747959443cbdfb06b6c5f176e35
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-08 19:29:11 +0100

    Add Role to application

--- EBookWeb/Program.cs
+++ EBookWeb/Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
