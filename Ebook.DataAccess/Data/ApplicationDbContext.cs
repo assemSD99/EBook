54fe3c345e9c9062b6c66993109d13cbc6115d61
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-11 14:09:22 +0100

    Create Oreder Header and Details Model

--- Ebook.DataAccess/Data/ApplicationDbContext.cs
+++ Ebook.DataAccess/Data/ApplicationDbContext.cs
@@ -6,9 +6,9 @@ namespace EBook.DataAccess
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
@@ -16,6 +16,8 @@ namespace EBook.DataAccess
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> applicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
-        public DbSet<ShoppingCart> ShoppingCarts { get; set;}
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
