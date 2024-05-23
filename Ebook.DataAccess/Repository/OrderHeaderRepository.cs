2574b0ce9b8c5097008221afd1de31ab13b99abd
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-23 22:39:26 +0100

    Loading Order Details Header

--- Ebook.DataAccess/Repository/OrderHeaderRepository.cs
+++ Ebook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -35,6 +35,7 @@ namespace Ebook.DataAccess.Repository
         public void UpdateStripePaymentID(int id, string sessionId, string paymenItentId)
         {
             var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            orderFromDb.PaymentDate = DateTime.Now;
             orderFromDb.SessionId = sessionId;
             orderFromDb.PaymentIntentId = paymenItentId;
 
