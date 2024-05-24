045ae330db16713c2f6123c5978c57e7f8dc5221
assemSD99 <133799856+assemSD99@users.noreply.github.com>
2024-05-24 16:24:02 +0100

    Cancel Order

--- EBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ EBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using Ebook.Models.ViewModel;
 using Ebook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using System.Security.Claims;
 
 namespace EBookWeb.Areas.Admin.Controllers
@@ -15,11 +16,11 @@ namespace EBookWeb.Areas.Admin.Controllers
         private readonly IUnitOfWork _unitOfWork;
         [BindProperty]
         public OrderVM OrderVM { get; set; }
-
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
             return View();
@@ -34,7 +35,77 @@ namespace EBookWeb.Areas.Admin.Controllers
             };
             return View(OrderVM);
         }
+
+        //[ActionName("Details")]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Details_PAY_NOW()
+        //{
+        //    OrderVM.OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
+        //    OrderVM.OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
+
+        //    //stripe settings 
+        //    var domain = "https://localhost:44300/";
+        //    var options = new SessionCreateOptions
+        //    {
+        //        PaymentMethodTypes = new List<string>
+        //        {
+        //          "card",
+        //        },
+        //        LineItems = new List<SessionLineItemOptions>(),
+        //        Mode = "payment",
+        //        SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderid={OrderVM.OrderHeader.Id}",
+        //        CancelUrl = domain + $"admin/order/details?orderId={OrderVM.OrderHeader.Id}",
+        //    };
+
+        //    foreach (var item in OrderVM.OrderDetail)
+        //    {
+
+        //        var sessionLineItem = new SessionLineItemOptions
+        //        {
+        //            PriceData = new SessionLineItemPriceDataOptions
+        //            {
+        //                UnitAmount = (long)(item.Price * 100),//20.00 -> 2000
+        //                Currency = "usd",
+        //                ProductData = new SessionLineItemPriceDataProductDataOptions
+        //                {
+        //                    Name = item.Product.Title
+        //                },
+
+        //            },
+        //            Quantity = item.Count,
+        //        };
+        //        options.LineItems.Add(sessionLineItem);
+
+        //    }
+
+        //    var service = new SessionService();
+        //    Session session = service.Create(options);
+        //    _unitOfWork.OrderHeader.UpdateStripePaymentID(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+        //    _unitOfWork.Save();
+        //    Response.Headers.Add("Location", session.Url);
+        //    return new StatusCodeResult(303);
+        //}
+
+        //public IActionResult PaymentConfirmation(int orderHeaderid)
+        //{
+        //    OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderHeaderid);
+        //    if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+        //    {
+        //        var service = new SessionService();
+        //        Session session = service.Get(orderHeader.SessionId);
+        //        //check the stripe status
+        //        if (session.PaymentStatus.ToLower() == "paid")
+        //        {
+        //            _unitOfWork.OrderHeader.UpdateStatus(orderHeaderid, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+        //            _unitOfWork.Save();
+        //        }
+        //    }
+        //    return View(orderHeaderid);
+        //}
+
         [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
         [ValidateAntiForgeryToken]
         public IActionResult UpdateOrderDetail()
         {
@@ -59,8 +130,8 @@ namespace EBookWeb.Areas.Admin.Controllers
             return RedirectToAction("Details", "Order", new { orderId = orderHEaderFromDb.Id });
         }
 
-
         [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
         [ValidateAntiForgeryToken]
         public IActionResult StartProcessing()
         {
@@ -70,8 +141,8 @@ namespace EBookWeb.Areas.Admin.Controllers
             return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
         }
 
-
         [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
         [ValidateAntiForgeryToken]
         public IActionResult ShipOrder()
         {
@@ -87,6 +158,35 @@ namespace EBookWeb.Areas.Admin.Controllers
             _unitOfWork.OrderHeader.Update(orderHeader);
             _unitOfWork.Save();
             TempData["Success"] = "Order Shipped Successfully.";
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public IActionResult CancelOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderHeader.PaymentIntentId
+                };
+
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            }
+            else
+            {
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+            }
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Order Cancelled Successfully.";
             return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
         }
 
@@ -106,6 +206,7 @@ namespace EBookWeb.Areas.Admin.Controllers
                 var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
                 orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
             }
+
             switch (status)
             {
                 case "pending":
@@ -123,6 +224,7 @@ namespace EBookWeb.Areas.Admin.Controllers
                 default:
                     break;
             }
+
 
             return Json(new { data = orderHeaders });
         }
